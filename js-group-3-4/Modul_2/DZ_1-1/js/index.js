20513aef932fe1a352a50182ca02008ea6c49a64
Sergii Tymchenko <p70smart@gmail.com>
2018-05-22 22:15:45 +0300

    Modul two

---
+++ js-group-3-4/Modul_2/DZ_1-1/js/index.js
@@ -0,0 +1,20 @@
+'use strict';
+
+
+const passwords = ['qwerty', '111qwe', '123123', 'qweqwe'];
+let userInput;
+let attempt = 3;
+
+while (userInput !== null && attempt !== 0) {
+	userInput = prompt('Введіть пароль');
+	attempt -= 1;
+	const hasLogin = passwords.includes(userInput);
+	if (hasLogin) {
+		alert(`Ти знаєш пароль вітаю`);
+		break;
+	} else if (attempt === 0) {
+		alert(`У Вас закінчилися спроби, акаунт заблоковано`);
+	} else {
+		alert(`Невірний пароль у Вас залишилося ${attempt}`);
+	}
+}
---
+++ js-group-3-4/Modul_2/DZ_1/js/index.js
@@ -0,0 +1,19 @@
+'use strict';
+
+let userInput;
+const number = [];
+let total = 0;
+
+while ( userInput !== null) {
+	userInput = prompt('Введіть число');
+	if ( !parseInt(userInput) ) {
+		alert('Ви ввели не число');
+	} else {
+		number.push(userInput);
+	}
+}
+for ( let item of number ) {
+	total += Number(item);
+}
+alert( `Загальна сумма всіх введених чисел ${total}` );
+
