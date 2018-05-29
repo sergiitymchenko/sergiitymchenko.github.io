5efabf52fd92440876856adb5a990712689bf538
Sergii Tymchenko <p70smart@gmail.com>
2018-05-29 17:44:20 +0300

    add dz modul_3

---
+++ js-group-3-4/Modul_3/js/index.js
@@ -0,0 +1,48 @@
+'use strict';
+
+const logins = ["Mango", "robotGoogles", "Poly", "Aj4x1sBozz", "qwerty123"];
+const userInput = prompt(`Введіть логін`);
+
+const checkInLoginExists = function () {
+
+	for (let item of logins) {
+		if ( userInput === item ) {
+			alert(`Такий логін вже використовується`);
+			return;
+		}
+	}
+	return false;
+};
+
+const checkLoginValidity = function () {
+	const min = 4;
+	const max = 16;
+	const inputLength = userInput.length;
+
+	if (inputLength >= min && inputLength <= max ) {
+		return;
+	} else {
+		alert(`Логін повинен бути від ${min} до ${max}`);
+		return;
+	}
+	return false;
+};
+
+const addLogin = function () {
+
+	if (userInput === null) {
+		return;
+	}
+
+const loginValidity = checkLoginValidity();
+const loginExists = checkInLoginExists();
+	
+	if ( loginExists === false ) {
+		logins.push(userInput);
+		alert(`Логін успішно добавлено`);
+	}
+	console.log(logins);
+
+};
+
+addLogin();
