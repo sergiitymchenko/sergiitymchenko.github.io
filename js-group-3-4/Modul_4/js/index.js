3b494cd354657c1fca4bf1958992ea7b0ecae548
Sergii Tymchenko <p70smart@gmail.com>
2018-06-09 13:30:43 +0300

    end homework modul_4

--- js-group-3-4/Modul_4/js/index.js
+++ js-group-3-4/Modul_4/js/index.js
@@ -10,6 +10,14 @@ const products = {
 	candy: 25
 };
 
+const order = {
+	chicken: 5,
+	pork: 8,
+	cheese: 6,
+	tea: 2,
+	candy: 2	
+};
+
 function Cashier(name, products) {
 	this.name = name;
 	this.products = products;
@@ -17,30 +25,52 @@ function Cashier(name, products) {
 	this.customerMoney = 0;
 	this.changeAmount = 0;
 
-	this.countTotalPrice = function (order) {
-		for (let key in order) {
-			const hasKey = order.hasOwnProperty(key);
-			if (hasKey) {
-				this.totalPrice += order[key];
+	this.countTotalPrice = function(order) {
+		const productsList = Object.keys(this.products);
+		const ordersList = Object.keys(order);
+		for (const product of ordersList) {
+			if (!productsList.includes(product)) {
+				alert(`Товара с именем ${product} нет`);
+				return null;
+			} else {
+				this.totalPrice += order[product] * products[product];
 			}
 		}
-		return this.totalPrice;
-	};
+	};	
 
-	this.getCustomerMoney = function (userInput, numberInput) {
-		while(userInput !== null) {
-			userInput = prompt(`Вам потрібно заплатити: ${this.totalPrice}`);
-			numberInput = Number(userInput);
-			if (numberInput >= this.totalPrice) {
-				numberInput = this.customerMoney;
-				return this.customerMoney;
+	this.getCustomerMoney = function () {
+		while(true) {
+			this.customerMoney = prompt(`Сумма покупок ${this.totalPrice}`);
+
+			if (this.customerMoney === null) {
+				return null;
 			}
-		}
-		return null;
-	};
+
+			if (this.customerMoney === '') {
+				alert(`Ви нічого не ввели`);
+			}
+
+			if ( Number.isNaN(Number(this.customerMoney)) ) {
+				alert(`Ви ввели не число`);
+			}
+
+			if (Number(this.customerMoney) < this.totalPrice) {
+				alert(`Ви ввели меньшу сумму`);
+			}
+
+			if (Number(this.customerMoney) >= this.totalPrice) {
+				this.customerMoney = Number(this.customerMoney);
+				return;
+			}
+
+			if (Number(this.customerMoney) === this.totalPrice) {
+				return;
+			}
+		};
+};
 
 	this.countChange = function () {
-		
+		this.changeAmount = Number(this.customerMoney) - this.totalPrice;
 	};
 
 	this.reset = function () {
@@ -50,19 +80,20 @@ function Cashier(name, products) {
 	};
 
 	this.serve = function (order) {
+		this.countTotalPrice(order);
 		this.getCustomerMoney();
-		this.countTotalPrice();
 
+		if (this.customerMoney === null ) {
+			alert(`Щось пішло не так, приходьте ще`);
+			return;
+		}
+
+		this.countChange();
+		alert(`Дякуємо за покупку, приходьте ще. Ваша решта ${this.changeAmount}`);
+		this.reset();
 	};
 };
-const poly = new Cashier('Poly', products);
 
-const order = {
-	chicken: 50,
-	pork: 80,
-	cheese: 60,
-	tea: 20,
-	candy: 25	
-};
+const poly = new Cashier('Poly', products);
 
 poly.serve(order);
