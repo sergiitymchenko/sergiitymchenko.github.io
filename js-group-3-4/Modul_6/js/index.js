644ae7c4352d515f5e58def587e0ca40b1f5632c
Sergii Tymchenko <p70smart@gmail.com>
2018-06-25 16:30:09 +0300

    add DZ_Modul_6

---
+++ js-group-3-4/Modul_6/js/index.js
@@ -0,0 +1,125 @@
+'use strict';
+
+class Hamburger {
+  constructor(size, stuffing) {
+    this.size = size;
+    this.stuffing = stuffing;
+    this.toppings = [];
+  }
+
+  addTopping(topping) {
+   if (!this.toppings.includes(topping)) {
+    this.toppings.push(topping);
+   }
+  }
+
+  removeTopping(topping) {
+    this.toppings = this.toppings.filter(elem => elem !== topping)
+  }
+
+  getToppings() {
+    return this.toppings;
+  }
+
+  getSize() {
+    return this.size;
+  }
+
+  getStuffing() {
+    return this.stuffing;
+  }
+
+  calculatePrice() {
+    const summSizePrice = Hamburger.SIZES[this.size].price;
+    const summStuffingPrice = Hamburger.STUFFINGS[this.stuffing].price;
+    const summToppingPrice = this.toppings.reduce( (acc, val) => acc + Hamburger.TOPPINGS[val].price, 0 );
+
+    const summTotalPrice = summSizePrice + summStuffingPrice +summToppingPrice;
+
+    return summTotalPrice;
+  }
+
+  calculateCalories() {
+    const summSizeCalc = Hamburger.SIZES[this.size].calories;
+    const summStuffingCalc = Hamburger.STUFFINGS[this.stuffing].calories;
+    const summToppingCalc = this.toppings.reduce( (acc, val) => Hamburger.TOPPINGS[val].calories, 0 );
+
+    const summTotalCalories = summSizeCalc + summStuffingCalc + summToppingCalc;
+    
+    return summTotalCalories;
+  }
+}
+
+Hamburger.SIZE_SMALL = 'SIZE_SMALL';
+Hamburger.SIZE_LARGE = 'SIZE_LARGE';
+
+Hamburger.SIZES = {
+  [Hamburger.SIZE_SMALL]: {
+    price: 30,
+    calories: 50,
+  },
+  [Hamburger.SIZE_LARGE]: {
+    price: 50,
+    calories: 100,
+  }
+};
+
+Hamburger.STUFFING_CHEESE = 'STUFFING_CHEESE';
+Hamburger.STUFFING_SALAD = 'STUFFING_SALAD';
+Hamburger.STUFFING_MEAT = 'STUFFING_MEAT';
+
+Hamburger.STUFFINGS = {
+  [Hamburger.STUFFING_CHEESE]: {
+    price: 15,
+    calories: 20,
+  },
+  [Hamburger.STUFFING_SALAD]: {
+    price: 20,
+    calories: 5,
+  },
+  [Hamburger.STUFFING_MEAT]: {
+    price: 35,
+    calories: 15,
+  },
+};
+					
+Hamburger.TOPPING_SPICE = 'TOPPING_SPICE';
+Hamburger.TOPPING_SAUCE = 'TOPPING_SAUCE';
+
+Hamburger.TOPPINGS = {
+  [Hamburger.TOPPING_SPICE]: {
+    price: 10,
+    calories: 0,
+  },
+  [Hamburger.TOPPING_SAUCE]: {
+    price: 15,
+    calories: 5,
+  },
+};
+
+// Маленький гамбургер с начинкой из сыра
+const hamburger = new Hamburger(Hamburger.SIZE_SMALL, Hamburger.STUFFING_CHEESE);
+
+// Добавка из приправы
+hamburger.addTopping(Hamburger.TOPPING_SPICE);
+
+// Спросим сколько там калорий
+console.log("Calories: ", hamburger.calculateCalories());
+
+// Сколько стоит?
+console.log("Price: ", hamburger.calculatePrice());
+
+// Я тут передумал и решил добавить еще соус
+hamburger.addTopping(Hamburger.TOPPING_SAUCE);
+
+// А сколько теперь стоит? 
+console.log("Price with sauce: ", hamburger.calculatePrice());
+
+// Проверить, большой ли гамбургер? 
+console.log("Is hamburger large: ", hamburger.getSize() === Hamburger.SIZE_LARGE); // -> false
+
+// Убрать добавку
+hamburger.removeTopping(Hamburger.TOPPING_SPICE);
+						     
+// Смотрим сколько добавок
+console.log("Hamburger has %d toppings", hamburger.getToppings().length); // 1
