42087a3038c5f103e7810dd03d7bd5a233690c90
Sergii Tymchenko <p70smart@gmail.com>
2018-09-07 17:02:02 +0300

    fixed modul_12

--- js-group-3-4/Modul_12/src/services/api.js
+++ js-group-3-4/Modul_12/src/services/api.js
@@ -1,12 +1,26 @@
-import axios from 'axios';
+// import axios from 'axios';
 
 const API_KEY = '5b8e43105ece6fba951fde2b9ba4ab490dfde8c487f55';
 
-export const fetchCard = (input ) => {
+// export const fetchCard = (input) => {
+// 	const url = `http://api.linkpreview.net/?key=${API_KEY}&q=${input}`;
+
+// 	return axios
+// 		.get(url)
+// 		.then(res => res.data)
+// 		.catch(error => console.log('axios err: ', error))
+// };
+
+
+const fetchCard= (input) => {
+
 	const url = `http://api.linkpreview.net/?key=${API_KEY}&q=${input}`;
 
-	return axios
-		.get(url)
-		.then(res => res.data)
-		.catch(error => console.log('axios err: ', error))
+	return fetch(url)
+	.then(response => {
+		if (response.ok) return response.json();
+		throw new Error('error: ' + response.statusText);
+	})
+	.then(data => data.hits)
+	.catch(error => console.log(error))
 };
