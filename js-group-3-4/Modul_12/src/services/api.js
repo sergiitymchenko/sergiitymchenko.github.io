1cfa2a426a690da97e90b023ca63caa9ceb83597
Sergii Tymchenko <p70smart@gmail.com>
2018-09-08 14:17:26 +0300

    fixed app.js modul_12

--- js-group-3-4/Modul_12/src/services/api.js
+++ js-group-3-4/Modul_12/src/services/api.js
@@ -3,7 +3,7 @@ import axios from 'axios';
 const API_KEY = '5b8e43105ece6fba951fde2b9ba4ab490dfde8c487f55';
 
 export const fetchCard = (input) => {
-	const url = `http://api.linkpreview.net/?key=${API_KEY}&q=${input}`;
+	const url = `https://api.linkpreview.net/?key=${API_KEY}&q=${input}`;
 
 	return axios
 		.get(url)
